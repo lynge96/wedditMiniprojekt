5fc4042967300569d4cf4fbe9998248eb3e01493
Anders <90785699+lynge96@users.noreply.github.com>
2022-10-04 12:53:52 +0200

    Blazor Virker, måske

--- wedditBlazor/Program.cs
+++ wedditBlazor/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using wedditBlazor;
 
@@ -9,3 +9,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
