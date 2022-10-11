eae2b05b0afb0bb64f22ca0d5c9ddfe0917836a8
Jacob Kaae <jacobkaae0511@gmail.com>
2022-10-11 10:36:15 +0200

    Prøv nu

--- wedditBlazor/Program.cs
+++ wedditBlazor/Program.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using wedditBlazor;
+using wedditBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ApiService>();
 
 await builder.Build().RunAsync();
 
---
+++ wedditBlazor/Services/ApiService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http.Json;
+using shared.Model;
+
+namespace wedditBlazor.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient http;
+        private readonly IConfiguration configuration;
+        private readonly string baseAPI = "";
+
+        public event Action RefreshRequired;
+
+        public ApiService(HttpClient http, IConfiguration configuration)
+        {
+            this.http = http;
+            this.configuration = configuration;
+            this.baseAPI = configuration["base_api"];
+        }
+
+        public async Task<Post[]> GetPosts()
+        {
+            string url = $"{baseAPI}posts/";
+            return await http.GetFromJsonAsync<Post[]>(url);
+        }
+    }
+}
+
